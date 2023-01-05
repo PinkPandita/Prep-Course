a192096d0b984c2c7463736202b70c922bfeafd8
stardustz439 <stardustz439@mail.com>
2023-01-05 18:37:03 -0500

    resolvi problemas jsii

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -109,7 +109,7 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-  if (numero - Math.floor(numero) == 0) {return true 
+  if (Math.floor(numero) == numero) {return true 
     
   } else { return false
     
@@ -156,30 +156,23 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-if (numero > 2) { return false
-  
-}
-  let raiz = Math.floor( Math.sqrt(numero)) +1;
-for(let i = 2; i < raiz; ++i) {
-  if (numero % i == 0) { 
-    return false;
+if (numero === 1 || numero === 0) return false;
+  for(let i = 2; i < numero; i++){
+    if(numero % i === 0)return false 
   }
-  return true
-}
-
+return true;
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-  if (valor === true) {return "Soy verdadero" 
-    
-  } else { return "Soy falso"
+  if (valor === true) return "Soy verdadero" 
+    return "Soy falso"
     
   }
 
-}
+
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
